6fe15b33aeafe3b8fa4877736884b623e517908e
AyaIkegami <aya.arashi.4235@gmail.com>
2026-01-15 22:30:33 +0900

    [feat]add movie entity

--- movie-app/src/main/java/com/example/movie_app/entity/Movie.java
+++ movie-app/src/main/java/com/example/movie_app/entity/Movie.java
@@ -1,8 +1,21 @@
 package com.example.movie_app.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class Movie {
+    @Id
+    int id;
 
+    String title;
+    int releasedYear;
+    String director;
+    String mainCast;
+    String country;
+    String distributor;
+
+    LocalDate watchedDate;
+    String comment;
 }
