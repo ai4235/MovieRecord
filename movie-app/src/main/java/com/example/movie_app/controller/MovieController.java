39e679fb196b614b529593748f69f2bef431d62e
AyaIkegami <aya.arashi.4235@gmail.com>
2026-04-12 09:56:54 +0900

    Connection check

--- movie-app/src/main/java/com/example/movie_app/controller/MovieController.java
+++ movie-app/src/main/java/com/example/movie_app/controller/MovieController.java
@@ -1,8 +1,19 @@
 package com.example.movie_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MovieController {
-    
+
+    @GetMapping("/")
+    public String moveToTop() {
+        return "redirect:/top";
+    }
+
+    @GetMapping("/top")
+    public String showTopPage() {
+        return "top";
+    }
+
 }
